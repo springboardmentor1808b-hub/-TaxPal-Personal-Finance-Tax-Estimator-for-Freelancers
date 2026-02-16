641c13dd8f0bd84cebae22b0ea8a895cdd6dba52
Sadhna Gupta <sadhnagupta7770@gmail.com>
2026-02-16 15:04:47 +0530

    Added backend with auth setup

---
+++ backend/src/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+
+
+// Security headers
+app.use(helmet());
+
+// Enable CORS
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error(" MongoDB Connection Failed:", err.message);
+    process.exit(1); // Exit if DB fails
+  });
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
