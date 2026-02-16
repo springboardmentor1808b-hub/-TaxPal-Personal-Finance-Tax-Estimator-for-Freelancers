1aba864144cf378faf4559693195beefc03a7821
sawantprerana <sawantprerana116@gmail.com>
2026-02-16 14:49:27 +0530

    Fixed Register UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/pages/Login'
-
+import Register from './components/pages/Register' 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {}
+        <Route path="/" element={<Register />} /> 
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   )
