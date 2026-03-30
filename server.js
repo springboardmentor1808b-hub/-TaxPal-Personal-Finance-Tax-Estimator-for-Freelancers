4ea518405b9e35a2ed1a40b7ba5b2be9731727eb
Anjali kumari <kumarianjali.kriti@gmail.com>
2026-03-30 20:38:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
+const errorHandler = require("./middleware/errorHandler");
+const transactionRoutes = require("./routes/transactionRoutes");
+const budgetRoutes = require("./routes/budgetRoutes");
+const taxRoutes = require("./routes/taxRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/budgets", budgetRoutes);
+app.use("/api/taxes", taxRoutes);
+
+// Test Routes
+app.get("/", (req, res) => {
+  res.send("TaxPal Backend Running 🚀");
+});
+
+// Protected Route
+app.get("/api/protected", protect, (req, res) => {
+  res.json({ message: "You accessed protected route", user: req.user });
+});
+
+// Error Handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error(`Unhandled Rejection: ${reason}`);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error(`Uncaught Exception: ${err.message}`);
+  process.exit(1);
+});
