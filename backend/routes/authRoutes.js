9b79237f4c5c4ca1607039c5c2276b405e236fc3
Ayush Mukherjee <120130371+IceHawk11@users.noreply.github.com>
2026-04-01 19:46:39 +0530

    Revert "files added"

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signup, login,updateProfile } = require("../controllers/authController");
-
-router.post("/signup", signup);
-router.post("/login", login);
-router.put("/update-profile", updateProfile);
-
-module.exports = router;
